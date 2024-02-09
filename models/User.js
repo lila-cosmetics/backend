6c4252c56dca3240f423969052208a1c37b183f4
naeemi7 <abdulwasenaeemi7@gmail.com>
2024-02-09 14:37:32 +0100

    The basic user schema has been created

---
+++ models/User.js
@@ -0,0 +1,27 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = model("User", userSchema);
+
+export default User;
