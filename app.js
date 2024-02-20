d657a64a2003cf5b008192f7e00ca13ca876f054
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2024-02-20 12:39:47 +0100

    bug fixed

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cookieParser());
 // Initializing CORS
 app.use(
   cors({
-    origin: "[http://localhost:3000]",
+    origin: "http://localhost:3000",
     methods: ["HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   })
